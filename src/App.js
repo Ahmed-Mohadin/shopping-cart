2dd8f0f71915c727004d5ffc47988db6ec6f8aa4
ahmed.mohadin <ahmed.mohadin@outlook.com>
2022-09-05 15:17:23 +0200

    Designed Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
 import './styles/style.css';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello App</h1>
+      <NavBar cartCount={999} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>
+        Built by
+        <a
+          className="footer-link"
+          href="https://github.com/Ahmed-Mohadin/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Ahmed Mohadin
+        </a>
+        |
+        <a
+          className="footer-link"
+          href="https://github.com/Ahmed-Mohadin/shopping-cart"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Source
+        </a>
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
